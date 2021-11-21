7dcec7868716e5a7f67068603ae14985560075a2
sramzi <sramzi@student.chalmers.se>
2021-11-21 17:27:20 +0100

    send the whole JSON to visualizer instead of coordinates

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var indexChecker = new Array();
 var takenCoordinatesRed = new Array();
 var availableCoordinatesRed = new Array();
 var stringTaken = "0";
+var allCoordinates = Locations.entireCoordinates();
+var allJSON = Locations.sendWholeJson();
 
 
 subscriber.start(); //starts the subscriber.js module
@@ -13,27 +15,41 @@ publisher.start(); //starts the publisher.js module
 
 subscriber.eventListener.on("mqttRecieved", function(topic, payload) {
     try {
+        console.log(payload.length)
         if (payload.length < 50) { // date payload length is a maximum of 27
             var dayName = Locations.extractDay(payload)
             var timeChosen = Locations.extractTime(payload)
             var booleanValue = Locations.validateTime(dayName, timeChosen)
 
             if (booleanValue != true) {
-                //     publisher.publish("no booking the time chosen is not valid by any dentist office"); // the visualize will give booking response of rejection
+                console.log("no booking the time chosen is not valid by any dentist office")
+                publisher.publish("no booking the time chosen is not valid by any dentist office"); // the visualize will give booking response of rejection
+            } else {
+                // var coordinatesInString = JSON.stringify(allCoordinates)
+                // var withoutQuotes = coordinatesInString.replace(/"/g, '');
+                // console.log(withoutQuotes)
+
+                var wholeJson = JSON.stringify(allJSON)
+                    //var wihtoutQuotesString = JSON.stringify(withoutQuotes)
+                    //console.log(wihtoutQuotesString)
+                publisher.publish(wholeJson)
             }
         } else {
             var dentistData = Locations.extractDentistData(payload) // booking payload load length is about 70
             var takenDate = Locations.storeChosenOnes(dentistData)
                 //console.log(takenDate)
             if (indexChecker.includes(takenDate)) {
+                console.log("the date is taken")
                 var takenCoordinates = Locations.extractDnetistCoordinates(payload)
                 takenCoordinatesRed.push(takenCoordinates)
                     //console.log(takenCoordinatesRed)
                 stringTaken = JSON.stringify(takenCoordinatesRed)
 
-                //   publisher.publish(stringTaken)
+                // publisher.publish(stringTaken) DONT UNCOMMENT
                 publisher.publish(JSON.stringify({ status: "Its already taken" }))
             } else {
+                console.log("the date is NOT taken")
+
                 indexChecker.push(takenDate)
                 console.log(indexChecker)
                 publisher.publish(JSON.stringify({
@@ -42,7 +58,6 @@ subscriber.eventListener.on("mqttRecieved", function(topic, payload) {
             }
 
 
-            var allCoordinates = Locations.entireCoordinates();
 
             var countKey = Object.keys(stringTaken).length; // how many elements in the array
             console.log(countKey)
@@ -52,7 +67,7 @@ subscriber.eventListener.on("mqttRecieved", function(topic, payload) {
                 // console.log(withoutQuotes)
 
                 var wihtoutQuotesString = JSON.stringify(withoutQuotes)
-                publisher.publish(wihtoutQuotesString)
+                    // publisher.publish(wihtoutQuotesString)
             } else {
 
                 for (var i = 0; i < takenCoordinatesRed.length; i++) {
@@ -66,7 +81,7 @@ subscriber.eventListener.on("mqttRecieved", function(topic, payload) {
                         var wihtoutQuotesString = JSON.stringify(withoutQuotes)
 
                         //console.log(coordinatesInString)
-                        publisher.publish(wihtoutQuotesString)
+                        // publisher.publish(wihtoutQuotesString)
                     }
                 }
             }
